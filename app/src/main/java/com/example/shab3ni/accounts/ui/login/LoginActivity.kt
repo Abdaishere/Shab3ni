0b8ef9a39f0b100f2bf8beb7d854e30a467a209a
Abdallah El belkasy <59124608+Abdaishere@users.noreply.github.com>
2022-08-15 16:52:32 +0200

    created files

---
+++ app/src/main/java/com/example/shab3ni/accounts/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shab3ni.accounts.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shab3ni.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/shab3ni/accounts/ui/register/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shab3ni.accounts.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shab3ni.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
